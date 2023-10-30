22fdbff869c7ea0a6da3375bc1489ea7ca0b2f28
Carlos Ordaz <carlos.ordaz@acomo.com.pe>
2023-10-29 22:30:37 -0500

    hh

--- src/utils/pdf/Schedule.js
+++ src/utils/pdf/Schedule.js
@@ -10,6 +10,11 @@ export const generatePdfSchedule = (payments, quotation) => {
     const pageSize = doc.internal.pageSize;
     const pageWidth = pageSize.width;
     const positionY = 100;
+    let fechaActual = new Date();
+    let dia = fechaActual.getDate().toString().padStart(2, '0');
+    let mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
+    let anio = fechaActual.getFullYear();
+    let fechaFormateada = `${dia}/${mes}/${anio}`;
     const colors = {
         black: [0, 0, 0],
         white: [255, 255, 255],
@@ -20,8 +25,9 @@ export const generatePdfSchedule = (payments, quotation) => {
     doc.setFont("helvetica", "bold");
     doc.setFontSize(10);
     doc.addImage('https://i.postimg.cc/KYNJ3tsn/logo.png', 'PNG', pageWidth / 2 - 20, 10, 40, 40, 'alias', 'FAST', 0);
+
     doc.text("PLAN DE PAGOS POR EL MÉTODO FRANCES VENCIDO ORDINARIO", pageWidth / 2, 65, { align: 'center' });
-    doc.text("Fecha: 21/10/2023", pageWidth - 70, 95, { align: 'center' });
+    doc.text(`Fecha: ${fechaFormateada}`, pageWidth - 70, 95, { align: 'center' });
     const symbol = quotation.currency == 'USD' ? '$' : 'S/.';
     const getDescripPeriGr = () => {
         let flows = payments;
@@ -53,9 +59,9 @@ export const generatePdfSchedule = (payments, quotation) => {
                 otro_t = false;
             }
         }
-        const formatPeriod = (period) => `[${period.inicio}${period.fin ? `-${period.fin}` : ''}]`;
-        const cadParcial = p.length ? `Parcial(${p.map(formatPeriod).join(',')})` : '';
-        const cadTotal = t.length ? `Total(${t.map(formatPeriod).join(',')})` : '';
+        const formatPeriod = (period) => `(${period.inicio}${period.fin ? `-${period.fin}` : ''})`;
+        const cadParcial = p.length ? `Parcial ${p.map(formatPeriod).join(',')}` : '';
+        const cadTotal = t.length ? `Total ${t.map(formatPeriod).join(',')}` : '';
         if (!cadParcial && !cadTotal) return 'No aplica';
         return `${cadParcial} ${cadTotal}`.trim();
     }
